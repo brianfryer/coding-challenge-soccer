56f8db994e4e2719d4e2ba89309370e57c3f667a
Brian Fryer <brianfryer@gmail.com>
2021-08-31 11:39:31 -0500

    Create constants.js

---
+++ commands/parseScoresFromFile/constants.js
@@ -0,0 +1,14 @@
+const POINT_VALUES = {
+  'win': 3,
+  'draw': 1,
+  'loss': 0,
+};
+
+const TEAM_SEPARATOR = ',';
+const SCORE_SEPARATOR = ' ';
+
+module.exports = {
+  POINT_VALUES,
+  TEAM_SEPARATOR,
+  SCORE_SEPARATOR,
+};
